ee28b60f0efd2e091cde2e621fb6fba9b324a0a4
Dara Seam <ouk.sovannratana19@kit.edu.kh>
2022-01-11 14:10:25 +0700

    first commit

---
+++ src/components/BlogList.js
@@ -0,0 +1,11 @@
+const BlogList = () => {
+    return (
+        <div className="blog-list">
+            <div className="blog-preview">
+                <h2>Blog Title</h2>
+                <p>Written by this Author</p>
+            </div>
+        </div>
+    )
+}
+export default BlogList;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import BlogList from "./BlogList";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <BlogList />
+        </div>
+    )
+}
+export default Home;
